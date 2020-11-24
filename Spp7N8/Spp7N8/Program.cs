1abd09e220220ecde52d7e0c889b0b26b096ed08
ambulatur <ambulatur@gmail.com>
2020-11-24 22:57:17 +0300

    Update 8

--- Spp7N8/Spp7N8/Program.cs
+++ Spp7N8/Spp7N8/Program.cs
@@ -1,32 +1,28 @@
 ﻿
-//Задача 7.
-//	Создать на языке C# статический метод класса Parallel.WaitAll, который:	
-//	- принимает в параметрах массив делегатов;
-//-выполняет все указанные делегаты параллельно с помощью пула потоков;
-//-дожидается окончания выполнения всех делегатов.	
-
-//	"Реализовать простейший пример использования метода Parallel.WaitAll.
-
-//Задача 8.
-//Создать на языке C# пользовательский атрибут с именем ExportClass, применимый только к классам, и реализовать консольную программу, которая:
-//- принимает в параметре командной строки путь к сборке .NET (EXE- или DLL-файлу);
-//-загружает указанную сборку в память;
-//-выводит на экран полные имена всех public -типов данных этой сборки, помеченные атрибутом ExportClass."	
 
 
 using System;
+using System.Linq;
+using System.Reflection;
+using System.Runtime.CompilerServices;
 using System.Threading;
 
 namespace Spp7N8
 {
-    delegate void Work();
-    delegate void NotWork();
+    //Задача 7.
+    //	Создать на языке C# статический метод класса Parallel.WaitAll, который:	
+    //	- принимает в параметрах массив делегатов;
+    //-выполняет все указанные делегаты параллельно с помощью пула потоков;
+    //-дожидается окончания выполнения всех делегатов.	
 
+    //	"Реализовать простейший пример использования метода Parallel.WaitAll.
+
+    delegate void Work();
     class Parallel
     {
         public static bool? WaitAll<T>(T array)
         {
-            var actions = (Work)(object)array; // Override GetInvocationList if u want (T)(object) not (Work)(object) ex. => interface -> waitall<t>(t array) where t:interface
+            var actions = (Work)(object)array;
             int count = actions.GetInvocationList().Length;
             using (ManualResetEvent resetEvent = new ManualResetEvent(false))
             {
@@ -44,16 +40,107 @@ namespace Spp7N8
             Console.WriteLine("Done all actions");
             return true;
         }
+    }
+    //Задача 8.
+    //Создать на языке C# пользовательский атрибут с именем ExportClass, применимый только к классам, и реализовать консольную программу, которая:
+    //- принимает в параметре командной строки путь к сборке .NET (EXE- или DLL-файлу);
+    //-загружает указанную сборку в память;
+    //-выводит на экран полные имена всех public -типов данных этой сборки, помеченные атрибутом ExportClass."	
+
+    [System.AttributeUsage(AttributeTargets.Class, Inherited = false, AllowMultiple = true)]
+    sealed class ExportClassAttribute : Attribute
+    {
+        public ExportClassAttribute([CallerFilePath] string file = null, [CallerLineNumber] int line = 0)
+        {
+            Console.WriteLine($"Exported at {line} in {file}");
+        }
+    }
+
+    [ExportClass]
+    class PublicClass1
+    {
+        public string prop;
+    }
+    [ExportClass]
+    class PublicClass2
+    {
 
     }
+    class PrivateClass3
+    {
+    }
+
+    class ExportHelper
+    {
+
+        public static void DisplayEachClass(string path)
+        {
+            var assembly = Assembly.LoadFrom(path);
+            var types = assembly.GetTypes();
+            foreach (var type in types)
+            {
+                foreach (var item in type.GetCustomAttributes(typeof(ExportClassAttribute), false))
+                {
+                    Console.WriteLine(type.FullName);
+                    //DisplayMethods(type);
+                     DisplayProperties(type);
+                    DisplayMembers(type);
+                    DisplayFields(type);
+                }
+            }
+        }
+        private static void DisplayMethods(Type type)
+        {
+            foreach (var method in type.GetMethods().Where(method => method.IsPublic))
+            {
+                Console.BackgroundColor = ConsoleColor.Green;
+                Console.WriteLine(method.Name);
+                Console.ResetColor();
+            }
+        }
+        private static void DisplayProperties(Type type)
+        {
+            foreach (var property in type.GetProperties())
+            {
+                Console.BackgroundColor = ConsoleColor.Yellow;
+                Console.WriteLine(property.Name);
+                Console.ResetColor();
+            }
+        }
+        private static void DisplayMembers(Type type)
+        {
+            foreach (var member in type.GetMembers())
+            {
+                Console.BackgroundColor = ConsoleColor.Red;
+                Console.WriteLine(member.Name);
+                Console.ResetColor();
+            }
+        }
+        private static void DisplayFields(Type type)
+        {
+            foreach (var field in type.GetFields())
+            {
+                Console.BackgroundColor = ConsoleColor.Blue;
+                Console.WriteLine(field.Name);
+                Console.ResetColor();
+            }
+        }
+       
+
+
+
+    }
+
     class Program
     {
+
         static void Main(string[] args)
         {
+            ExportHelper.DisplayEachClass(args[0]);
+
             Work pseudoArray = new Work(Hello) + DisplayThread + new Work(Done); // Delegate have INBUILT ADRDESS ARRAY not [], but u can use it if u want, to net nikakoy raznica
             bool? fun = Parallel.WaitAll(pseudoArray) ?? false;
-            if ((bool)!fun ^ (fun??=false?true:false))
-                Console.WriteLine("Main method done after WaitAll");
+            Console.WriteLine("Main method done after WaitAll");
         }
         private static void Hello()
         {
